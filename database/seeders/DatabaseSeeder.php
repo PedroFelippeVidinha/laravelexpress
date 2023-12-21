16523c2391d3b2fabc692e9276a4980dfdda193c
Pedro Felippe Gonçalves Vidinha de Oliveira <pedrovidinha@MacBook-Air-de-Pedro.local>
2023-12-21 19:46:32 -0300

    first commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+
+        $this->call([
+            AddressSeeder::class,
+            CompanySeeder::class,
+            UserSeeder::class,            
+        ]);
+
+        DB::unprepared("REFRESH MATERIALIZED VIEW sales_commission_view");
+        
+        // \App\Models\User::factory(10)->create();
+
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+    }
+}
